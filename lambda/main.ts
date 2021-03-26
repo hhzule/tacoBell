b6f8cf7a4390ecfe320852893189d9f63ef9e0c0
hhzule <you@example.com>
2021-03-27 04:08:05 +0500

    final

---
+++ lambda/main.ts
@@ -0,0 +1,69 @@
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event: any) => {
+    console.log(event);
+    const body = JSON.parse(event.body);
+
+    switch (event.info.fieldName) {
+        case 'getCurrent':
+            // code to get current weather, and store in DynamoDB
+             try {
+                 const temp = Math.floor(Math.random() * 50 + 10) // dummy temperature
+                 const year = new Date().getFullYear(); // current year to make unique id with zipcode
+
+                // get current date
+                 let today: any = new Date();
+                 let dd = String(today.getDate()).padStart(2, '0');
+                let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+                let yyyy = today.getFullYear();
+                today = mm + '/' + dd + '/' + yyyy;
+ 
+         const params = {
+             TableName: process.env.TODOS_TABLE,
+             Key: {
+                 id: `${event.arguments.zipcode}${year}` // date as unique id
+             },
+             ExpressionAttributeNames: { "#city": `${today.toString()}` },
+             ExpressionAttributeValues: { ":city": `${temp}` },
+             UpdateExpression: "set #city = :city",
+             ReturnValues: "UPDATED_NEW"
+         };
+         const todos = await docClient.update(params).promise();
+         return temp;
+     }
+     catch (err) {
+         console.log("ERRROR", err);
+         return err;
+     }
+        case 'getData':
+            // code
+            try {
+           const year = event.arguments.input.year
+           const zipcode = event.arguments.input.zipcode
+           const key = zipcode + year;
+               const params = {
+             TableName: process.env.TODOS_TABLE,
+             Key: {
+                 id: key
+             }
+         };
+ 
+          console.log(key, "key")
+     
+           const todos = await docClient.get(params).promise();
+           return JSON.stringify(todos.Item)
+           
+        } catch (err) {
+                console.log("ERRROR", err);
+         return err;
+        }
+        default:
+            return "nothing"
+    }
+}
+
+// "{\"24-3-2021\":\"22\",\"id\":\"22222021\",\"25-3-2021\":\"27\"}"
+
+
+   
--- lib/sir-stack.ts
+++ lib/sir-stack.ts
@@ -1,9 +1,60 @@
 import * as cdk from '@aws-cdk/core';
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
+// import * as apigw from "@aws-cdk/aws-apigateway";
+import * as iam from '@aws-cdk/aws-iam';
+import * as appsync from '@aws-cdk/aws-appsync';
 
 export class SirStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
+    const api = new appsync.GraphqlApi(this, 'Api', {
+      name: 'appsync-api',
+      schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
+    });
+
+    const todosTable = new ddb.Table(this, "weather", {
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: "id",
+        type: ddb.AttributeType.STRING,
+      },
+    });
+
+    const todosLambda = new lambda.Function(this, "TodoApiHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "main.handler",
+      code: lambda.Code.fromAsset("lambda"),
+      memorySize: 1024,
+      environment: {
+        TODOS_TABLE: todosTable.tableName,
+
+      },
+      // layers : [lambdaLayerGenerateId,lambdaLayerVerifyTenant]
+    });
+
+    todosLambda.addToRolePolicy(new iam.PolicyStatement({
+      resources: ["*"],
+      actions: ["dynamodb:*"]}))
+
+todosTable.grantFullAccess(todosLambda)
+
+const lambdaDs = api.addLambdaDataSource('lambdaDatasource', todosLambda);
+
+lambdaDs.createResolver({
+  typeName: "Query",
+  fieldName: "getCurrent"
+});
+
+lambdaDs.createResolver({
+  typeName: "Query",
+  fieldName: "getData"
+});
+
+new cdk.CfnOutput(this, "GraphQLAPIURL", {
+  value: api.graphqlUrl
+});
   }
 }
