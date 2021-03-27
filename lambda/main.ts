a34774c002c4a8c08a43934b03f380c228505555
hhzule <you@example.com>
2021-03-27 06:41:40 +0500

    weather api implimented

--- lambda/main.ts
+++ lambda/main.ts
@@ -1,41 +1,67 @@
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
+import axios from "axios"
 
 exports.handler = async (event: any) => {
     console.log(event);
-    const body = JSON.parse(event.body);
+  
 
     switch (event.info.fieldName) {
         case 'getCurrent':
+            try{
             // code to get current weather, and store in DynamoDB
-             try {
-                 const temp = Math.floor(Math.random() * 50 + 10) // dummy temperature
-                 const year = new Date().getFullYear(); // current year to make unique id with zipcode
 
+            // first get the coordinates using zipcode
+                let lat;
+                let lng;
+                const getcity = await axios.get(`https://www.zipcodeapi.com/rest/X1paJY65GOw2Rv9pGbkIIuqzpwBEZOuiuXhhPnfFbIad4HB3kqF70rL2qBiLFpHM/info.json/${event.arguments.zipcode}/degrees`)
+                    .then(function (response) {
+                  
+                    lat = response.data.lat;
+                    lng = response.data.lng
+                });
+
+                // get weather using coordinates
+                let result;
+                    const getweather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=a1c75fcafe88cc8d0c19e0ff95b22d83`)
+                    .then(function (response) {
+                    console.log(response.data.main.temp, "weahther");
+                  result = response.data.main.temp
+                });
+
+                // const temp = Math.floor(Math.random() * 50 + 10); // dummy temperature
+
+                // current year to make unique id with zipcode
+                const year = new Date().getFullYear();
                 // get current date
-                 let today: any = new Date();
-                 let dd = String(today.getDate()).padStart(2, '0');
+                let today: any = new Date();
+                let dd = String(today.getDate()).padStart(2, '0');
                 let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
                 let yyyy = today.getFullYear();
                 today = mm + '/' + dd + '/' + yyyy;
+
+                // params object for Dynamo1DB
+                const params = {
+                    TableName: process.env.TODOS_TABLE,
+                    Key: {
+                        id: `${event.arguments.zipcode}${year}` // date as unique id
+                    },
+                    ExpressionAttributeNames: { "#city": `${today.toString()}` },
+                    ExpressionAttributeValues: { ":city": `${result}` },
+                    UpdateExpression: "set #city = :city",
+                    ReturnValues: "UPDATED_NEW"
+                };
+
+                // add to dy1namoDB
+                const todos = await docClient.update(params).promise();
+                // return the weather
+                return `${result}°C`;
+            }
+            catch (err) {
+                console.log("ERRROR", err);
+                return err;
+            }
  
-         const params = {
-             TableName: process.env.TODOS_TABLE,
-             Key: {
-                 id: `${event.arguments.zipcode}${year}` // date as unique id
-             },
-             ExpressionAttributeNames: { "#city": `${today.toString()}` },
-             ExpressionAttributeValues: { ":city": `${temp}` },
-             UpdateExpression: "set #city = :city",
-             ReturnValues: "UPDATED_NEW"
-         };
-         const todos = await docClient.update(params).promise();
-         return temp;
-     }
-     catch (err) {
-         console.log("ERRROR", err);
-         return err;
-     }
         case 'getData':
             // code
             try {
@@ -74,5 +100,6 @@ exports.handler = async (event: any) => {
 }
 
 
-
+// http://www.zipcodeapi.com/rest/X1paJY65GOw2Rv9pGbkIIuqzpwBEZOuiuXhhPnfFbIad4HB3kqF70rL2qBiLFpHM/info.json/75600/radians"
    
+// X1paJY65GOw2Rv9pGbkIIuqzpwBEZOuiuXhhPnfFbIad4HB3kqF70rL2qBiLFpHM
